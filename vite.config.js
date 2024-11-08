059c48acc32df93960077300a0ff0bf8f3b9636a
Michael Caballero <mcaballero.dev@gmail.com>
2024-11-08 11:08:10 -0800

    Add packages and configure proxy servers for microservices

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api/email": {
+				target: "http://localhost:3001",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/email/, ""),
+			},
+			"/api/markdown": {
+				target: "http://localhost:3002",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/markdown/, ""),
+			},
+			"/api/crud": {
+				target: "http://localhost:3003",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/crud/, ""),
+			},
+		},
+	},
+});
+
